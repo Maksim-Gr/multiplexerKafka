e1e35a6a91d0d39bf81fcb651b1cca99deec0e0f
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2023-11-06 14:09:47 +0300

    chore: introduce vars

--- cmd/consumer/consumer.go
+++ cmd/consumer/consumer.go
@@ -1,1 +1,9 @@
 package main
+
+// Temporary vars for testing
+const (
+	ConsumerGroup      = "multiplexer-group"
+	ConsumerTopic      = "notifications"
+	ConsumerPort       = ":8081"
+	KafkaServerAddress = "localhost:9092"
+)
